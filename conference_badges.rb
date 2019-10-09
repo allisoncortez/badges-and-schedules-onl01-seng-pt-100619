4cfb94e4f152fd926ef2d4b861100c82ad09744a
allison cortez <allizetorc@gmail.com>
2019-10-09 20:44:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -30,7 +30,7 @@ def assign_rooms(attendee)
 end
 
 def printer(attendee)
-  batch_badge_creator(names).each { |i| puts i }
+  badges.each { |names| puts names }
   assign_rooms(attendee).each {|room| puts room }
 end
 
