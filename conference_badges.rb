685e28b8d7ebec34849a77b23702019138497d90
allison cortez <allizetorc@gmail.com>
2019-10-09 18:45:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+# def badge_maker(name)
+#   badge_name = []
+#   counter = 0
+#   while counter <= badge_name.length
+#     puts "Hello, my name is #{name}."
+#     counter += 1
+#   end
+#   badge_name
+# end
+
+def batch_badge_creator(array)
+  #badge_name = []
+  counter = 0
+  batch_badge_creator(array).each do |array|
+  puts "Hello, my name is #{name}."
+    counter += 1
+  end
+end
+
