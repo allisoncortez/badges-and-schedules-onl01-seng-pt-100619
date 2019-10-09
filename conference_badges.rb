46f35b134480d3db57cc8a4a478c1abda6ef3271
allison cortez <allizetorc@gmail.com>
2019-10-09 19:25:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,6 +16,7 @@ def batch_badge_creator(array)
   array = [ "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz" ]
   array.each do |name|
     puts "Hello, my name is #{name}."
+  end
 end
 
 
